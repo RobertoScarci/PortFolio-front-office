eed674a63422557ea067ccdb7db9aa05b3cd03e6
RobertoScarci <robertoscarci.02@gmail.com>
2024-06-25 16:29:44 +0200

    first change

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,33 +7,32 @@
   <div id="app">
   <!-- Header -->
   <header class="text-center">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="https://banner2.cleanpng.com/20180708/bs/kisspng-bacardi-cocktail-martini-cocktail-glass-logo-cocktail-logo-5b42630719a652.9189521615310773831051.jpg" alt="Logo" width="50" height="40" class="d-inline-block align-text-top"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cocktails</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Chi Siamo</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contattaci</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="https://t4.ftcdn.net/jpg/04/35/60/49/360_F_435604903_8bWKQf9y1MWYBeG2o5UjwlHHWW8Gl6Kk.jpg" alt="Logo" width="100" height="60" class="d-inline-block align-text-top"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Risultati</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Chi Siamo</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contattaci</a>
+                    </li>
+                  </ul>
+                  <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  </form>
+            </div>
+      </div>
 </nav>
 
   </header>
@@ -60,12 +59,11 @@
 
 body {
       background-color: #f8f9fa;
-      font-family: Arial, sans-serif;
+      font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
     header {
-      background-color: #343a40;
       color: #fff;
-      padding: 60px 0;
+      padding: 0px 0;
     }
     header h1 {
       font-size: 48px;
