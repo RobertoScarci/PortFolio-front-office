3d03e9a2a447cdf13670bc320aa1addec58f02c0
RobertoScarci <robertoscarci.02@gmail.com>
2024-07-18 12:14:13 +0200

    add card for client's results in main section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,15 +11,12 @@
   <section>
       <div id="carouselExample" class="carousel slide container">
               <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img src="https://media.gqitalia.it/photos/5d935254b1322f0008a46f71/16:9/w_2560%2Cc_limit/GettyImages-655781960.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://www.projectinvictus.it/wp-content/uploads/2022/08/risultati-palestra-scaled.jpg" class="d-block w-100" alt="...">
-                </div>
-                <div class="carousel-item">
-                  <img src="https://www.fitonyou.it/wp-content/themes/fitonyou23/images/allenamento-con-personal-trainer-roma-palestra-fit-on-you.jpg" class="d-block w-100" alt="...">
-                </div>
+                  <div class="carousel-item active">
+                    <img src="https://media.gqitalia.it/photos/5d935254b1322f0008a46f71/16:9/w_2560%2Cc_limit/GettyImages-655781960.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://www.projectinvictus.it/wp-content/uploads/2022/08/risultati-palestra-scaled.jpg" class="d-block w-100" alt="...">
+                  </div>
               </div>
               <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon arrow" aria-hidden="true"></span>
@@ -34,10 +31,34 @@
 
   <!-- Risultati -->
   <section>
-    <div class="container">
+    <div class="container pt-5">
       <div class="row">
-        <div class="col-2">
-          
+        <div class="col-4">
+          <div class="card" style="">
+            <img src="https://eusebio.pro/wp-content/uploads/2022/03/ANDREA.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card" style="">
+            <img src="https://eusebio.pro/wp-content/uploads/2022/03/ANDREA.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card" style="">
+            <img src="https://eusebio.pro/wp-content/uploads/2022/03/ANDREA.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title text-center">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
