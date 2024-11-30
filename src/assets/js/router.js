fba4c5be4f917d900e78afeb02669f30fb838559
RobertoScarci <robertoscarci.02@gmail.com>
2024-11-30 09:57:40 +0100

    add new router

--- src/assets/js/router.js
+++ src/assets/js/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../../components/HomePage.vue'
 import Blog1 from '../../components/Blog1.vue'
+import Blog2 from '../../components/Blog2.vue'
+import Blog3 from '../../components/Blog3.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,7 +17,17 @@ const router = createRouter({
       path: '/blog1',
       name: 'blog1',
       component: Blog1,
-    }
+    },
+    {
+      path: '/blog2',
+      name: 'blog2',
+      component: Blog2,
+    },
+    {
+      path: '/blog3',
+      name: 'blog3',
+      component: Blog3,
+    },
   ]
 })
 
