55031702d2b85157fbb7d3de63313df0318d37f6
RobertoScarci <robertoscarci.02@gmail.com>
2024-12-01 11:40:28 +0100

    some changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 
 import HomePage from './components/HomePage.vue'
 import Blog1 from './components/Blog1.vue';
+import Blog2 from './components/Blog2.vue';
+import Blog3 from './components/Blog3.vue';
 
 export default{
-    components: { HomePage, Blog1 }
+    components: { HomePage, Blog1, Blog2, Blog3 }
 }
 
 
