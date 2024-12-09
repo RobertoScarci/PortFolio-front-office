ba11319cca7c37c66eb63ebb57f55881762fa856
RobertoScarci <robertoscarci.02@gmail.com>
2024-12-09 18:05:41 +0100

    change structure in PortFolio.vue

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -34,10 +34,9 @@
             <div class="container-main single-main">
               <div class="col-md-12">
                 <div class="block-main mb-30">
-                  <img src="../assets/img/images/blog-post-big.jpg" class="img-responsive w-100" alt="reviews2">
                   <div class="content-main single-post padDiv">
                     <div class="journal-txt">
-                      <h4><a href="#">SO LETS MAKE THE MOST IS BEAUTIFUL</a></h4>
+                      <h4><a href="#">MY PORTFOLIO</a></h4>
                     </div>
                     <div class="post-meta">
                       <ul class="list-unstyled mb-0">
@@ -58,81 +57,6 @@
                       1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
                       "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
                   </div>
-                </div>
-              </div>
-              <div class="col-md-12">
-                <div class="comments text-left padDiv mb-30">
-                  <div class="entry-comments">
-                    <h6 class="mb-30">4 comments</h6>
-                    <ul class="entry-comments-list list-unstyled">
-                      <li>
-                        <div class="entry-comments-item">
-                          <img src="../assets/img/images/avatar.jpg" class="entry-comments-avatar" alt="">
-                          <div class="entry-comments-body">
-                            <span class="entry-comments-author">Sommer Christian</span>
-                            <span><a href="#">fev 14, 2018 at 12:48 pm</a></span>
-                            <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam justo, ullamcorper tincidunt pellentesque in, condimentum ut enim. Aenean at pharetra diam, quis vulputate urna. </p>
-                            <a class="rep" href="#">Reply</a>
-                          </div>
-                        </div>
-                        <ul class="entry-comments-reply list-unstyled">
-                          <li>
-                            <div class="entry-comments-item">
-                              <img src="../assets/img/images/avatar.jpg" class="entry-comments-avatar" alt="">
-                              <div class="entry-comments-body">
-                                <span class="entry-comments-author">Sara Smith</span>
-                                <span><a href="#">fev 14, 2018 at 12:51 pm</a></span>
-                                <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam justo, ullamcorper tincidunt pellentesque in, condimentum ut enim. Aenean at pharetra diam, quis vulputate urna.</p>
-                                <a class="rep" href="#">Reply</a>
-                              </div>
-                            </div>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <div class="entry-comments-item">
-                          <img src="../assets/img/images/avatar.jpg" class="entry-comments-avatar" alt="">
-                          <div class="entry-comments-body">
-                            <span class="entry-comments-author">Andrew Lupkin</span>
-                            <span><a href="#">fev 14, 2018 at 12:55 pm</a></span>
-                            <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam justo, ullamcorper tincidunt pellentesque in, condimentum ut enim. Aenean at pharetra diam, quis vulputate urna. </p>
-                            <a class="rep" href="#">Reply</a>
-                          </div>
-                        </div>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-12">
-                <div class="cmt padDiv">
-                  <form id="comment-form" method="post" action="" role="form">
-                    <div class="row">
-                        <div class="col-lg-6">
-                          <div class="form-group">
-                            <input id="form_name" type="text" name="name" class="form-control" placeholder="Name *" required="required">
-                          </div>
-                        </div>
-                        <div class="col-lg-6">
-                          <div class="form-group">
-                            <input id="form_email" type="email" name="email" class="form-control" placeholder="email *" required="required">
-                          </div>
-                        </div>
-                        <div class="col-md-12">
-                          <div class="form-group">
-                            <input id="form_name" type="text" name="website" class="form-control" placeholder="Website">
-                          </div>
-                        </div>
-                        <div class="col-lg-12">
-                          <div class="form-group">
-                            <textarea id="form_message" name="message" class="form-control" placeholder="Message *" style="height: 200px;" required="required"></textarea>
-                          </div>
-                        </div>
-                        <div class="col-lg-12">
-                          <input type="submit" class="btn btn-defeault btn-send" value="Send message">
-                        </div>
-                    </div>
-                  </form>
                 </div>
               </div>
             </div>
