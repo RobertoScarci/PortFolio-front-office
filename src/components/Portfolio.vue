5481c6800026965c3323f24cc7a6894d5000842b
RobertoScarci <robertoscarci.02@gmail.com>
2024-12-14 10:01:07 +0100

    add h3 in portfolio.vue

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -44,6 +44,11 @@
                         <li class="date">date:<a href="#">November 31, 2024</a></li>
                       </ul>
                     </div>
+                    <div>
+                      <h3>
+                        BoolBnB
+                      </h3>
+                    </div>
                     <p class="mb-30">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to
                       using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
                       sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
