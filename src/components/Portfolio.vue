51e9eab758a8b3e9c9be4a5e064b6946065c50f5
RobertoScarci <robertoscarci.02@gmail.com>
2024-12-15 12:01:49 +0100

    change inside blockquote

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -36,7 +36,7 @@
                 <div class="block-main mb-30">
                   <div class="content-main single-post padDiv">
                     <div class="journal-txt text-center p-5">
-                      <h4>MY Projects</h4>
+                      <h4>MY PROJECTS</h4>
                     </div>
                     <div class="post-meta">
                       <ul class="list-unstyled mb-0">
@@ -50,7 +50,9 @@
                     <p class="mb-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                       specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
                       and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    <blockquote><img src="../assets/img/images/avatar.jpg" alt=""></blockquote>
+                    <blockquote><p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quo pariatur, ullam nihil voluptatem aliquam distinctio cumque nulla unde ut eos aut, commodi nesciunt nobis consequatur delectus provident porro excepturi.
+                    </p></blockquote>
                     <p class="mb-30">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
                       Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
                       1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
