1f031464bb0f7cbef481ab766cda2ac3dcdf10e0
RobertoScarci <robertoscarci.02@gmail.com>
2025-01-03 12:40:53 +0100

    add portfolio.vue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@ import HomePage from './components/HomePage.vue'
 import Blog1 from './components/Blog1.vue';
 import Blog2 from './components/Blog2.vue';
 import Blog3 from './components/Blog3.vue';
+import Portfolio from './components/Portfolio.vue';
 
 export default{
-    components: { HomePage, Blog1, Blog2, Blog3 }
+    components: { HomePage, Blog1, Blog2, Blog3, Portfolio }
 }
 
 
