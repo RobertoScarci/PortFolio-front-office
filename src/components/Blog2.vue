4619d3a04487961190e3bb1df7a5432e766013be
RobertoScarci <robertoscarci.02@gmail.com>
2025-01-10 10:52:23 +0100

    edit date comments

--- src/components/Blog2.vue
+++ src/components/Blog2.vue
@@ -73,7 +73,7 @@
                               <img src="../assets/img/images/avatar.jpg" class="entry-comments-avatar" alt="">
                               <div class="entry-comments-body">
                                 <span class="entry-comments-author">Sara Smith</span>
-                                <span><a href="#">fev 14, 2018 at 12:51 pm</a></span>
+                                <span><a href="#">jan 14, 2025 at 12:51 pm</a></span>
                                 <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam justo, ullamcorper tincidunt pellentesque in, condimentum ut enim. Aenean at pharetra diam, quis vulputate urna.</p>
                                 <a class="rep" href="#">Reply</a>
                               </div>
