8503a8155f1b2fd49defb6c78e3446307fc3df2c
RobertoScarci <robertoscarci.02@gmail.com>
2025-01-30 12:27:29 +0100

    edit data in Blog1

--- src/components/Blog1.vue
+++ src/components/Blog1.vue
@@ -42,7 +42,7 @@
                     <div class="post-meta">
                       <ul class="list-unstyled mb-0">
                         <li class="author">by:<a href="#">medsign</a></li>
-                        <li class="date">date:<a href="#">March 31, 2017</a></li>
+                        <li class="date">date:<a href="#">January 31, 2025</a></li>
                         <li class="commont"><i class="ion-ios-heart-outline"></i><a href="#">3 Comments</a></li>
                       </ul>
                     </div>
