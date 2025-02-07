9ef9a87df1c0714588aba3e662711d61466472e3
RobertoScarci <robertoscarci.02@gmail.com>
2025-02-07 12:30:14 +0100

    removed a paragraph in Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -53,10 +53,7 @@
                     <blockquote><p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quo pariatur, ullam nihil voluptatem aliquam distinctio cumque nulla unde ut eos aut, commodi nesciunt nobis consequatur delectus provident porro excepturi.
                     </p></blockquote>
-                    <p class="mb-30">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
-                      Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
-                      1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,
-                      "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+
                   </div>
                 </div>
               </div>
