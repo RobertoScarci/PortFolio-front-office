46f21c4b475b7a4f042b8f63075e12b2ee10a1c1
RobertoScarci <robertoscarci.02@gmail.com>
2025-02-18 12:30:38 +0100

    p decreased

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -48,8 +48,7 @@
                       using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
                       sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                     <p class="mb-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-                      specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                      and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                      specimen book. </p>
                     <blockquote><p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quo pariatur, ullam nihil voluptatem aliquam distinctio cumque nulla unde ut eos aut, commodi nesciunt nobis consequatur delectus provident porro excepturi.
                     </p></blockquote>
