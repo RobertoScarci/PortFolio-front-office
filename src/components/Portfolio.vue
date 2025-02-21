a5df670e006198696d4f05903803bdbbf9f51d44
RobertoScarci <robertoscarci.02@gmail.com>
2025-02-21 11:25:53 +0100

    add div for blockquote

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -89,7 +89,9 @@
                       sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
                     <p class="mb-30">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
                       specimen book. </p>
-                    <blockquote class=""><img src="../assets/img/images/avatar.jpg" alt=""></blockquote>
+                    <div class="justify-content-center">
+                      <blockquote class=""><img src="../assets/img/images/avatar.jpg" alt=""></blockquote>
+                    </div>
                     <p class="mb-30">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in
                       Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
                       1.10.32 and 1.10.33 of </p>
