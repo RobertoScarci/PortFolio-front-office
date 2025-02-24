c1b37b30068b10f1a0404c9ae5a019cc9c9eb6f9
RobertoScarci <robertoscarci.02@gmail.com>
2025-02-24 12:31:56 +0100

    changes for entry-comments in blog3

--- src/components/Blog3.vue
+++ src/components/Blog3.vue
@@ -63,14 +63,13 @@
               <div class="col-md-12">
                 <div class="comments text-left padDiv mb-30">
                   <div class="entry-comments">
-                    <h6 class="mb-30">4 comments</h6>
                     <ul class="entry-comments-list list-unstyled">
                       <li>
                         <div class="entry-comments-item">
                           <img src="../assets/img/images/avatar.jpg" class="entry-comments-avatar" alt="">
                           <div class="entry-comments-body">
-                            <span class="entry-comments-author">Sommer Christian</span>
-                            <span><a href="#">fev 14, 2018 at 12:48 pm</a></span>
+                            <span class="entry-comments-author">Roberto Scarci</span>
+                            <span><a href="#">feb 14, 2025 at 12:48 pm</a></span>
                             <p class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam justo, ullamcorper tincidunt pellentesque in, condimentum ut enim. Aenean at pharetra diam, quis vulputate urna. </p>
                             <a class="rep" href="#">Reply</a>
                           </div>
